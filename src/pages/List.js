d3f698c7fd0b722a39eba10803e68be302ff96f6
Roberto Gomes <robertohgm@hotmail.com>
2020-01-15 23:07:02 -0300

    Aplicação mobile

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
 
     useEffect(() => {
+        // AsyncStorage.removeItem('user');
+
         AsyncStorage.getItem('techs').then(storageTechs => {
             const techsArray = storageTechs.split(',').map(tech => tech.trim());
 
@@ -12,5 +18,27 @@ export default function List() {
         })
     }, []);
 
-return <Text>{techs}</Text>
+    return (
+        //SafeAreaView - conteúdo não fica por baixo de relógio
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: "contain",  //conteúdo da imagem fica dentro do espaço disponível,
+        alignSelf: 'center',
+        marginTop: 40
+    }
+});
